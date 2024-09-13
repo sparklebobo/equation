369a7b5d21dd7f132459274eed27bd7876a971b3
sparklebobo <sparkle_github@163.com>
2024-09-13 19:23:44 +0800

    Update main.py
    修复问题

--- main.py
+++ main.py
@@ -1,65 +1,56 @@
 
 
-def guess(mixn,maxn):
-    '''
+def guess(mixn,maxn):#这个函数用于根据给定的范围猜测数字
     if mixn == maxn:
         return mixn
     if mixn > maxn:
         mixn , maxn = maxn , mixn
     number = maxn - mixn + 1
     if number%2 == 0:
-        return int(number//2)
+        result = int(number//2)+mixn-1
     else:
-        return int((number+1)//2)
-    '''
-    if mixn == maxn:
-        return mixn
-    if mixn > maxn:
-        mixn , maxn = maxn , mixn
-    number = maxn - mixn + 1
-    if number%2 == 0:
-        return int(number//2)
-    else:
-        return int((number+1)//2)
+        result = int((number+1)//2)+mixn-1
+    return result
 
-def replace(minn,maxn,guess,result):#booln:1.guess<key:True 2.guess>key:False
-    if result=='ok':
-        return guess,maxn
-    if result:
-        minn=guess
-        pass
-    else:
-        maxn=guess
-        pass
-    return minn,maxn
-def check(guess):
+
+def replace(mixn,maxn,guess,result):#这个函数用于更新猜测范围
+    '''
+    输出结果为 最小值，最大值
+    '''
+    if result == 2:
+        return mixn,guess-1
+    elif result == 1:
+        return guess+1,maxn
+
+
+def check(guess_number):#这个函数用于检查猜测结果
+    '''
+    输出逻辑:
+    猜测结果>key,返回1
+    猜测结果<key,返回2
+    猜测结果=key,返回0
+    '''
     key=10
-    if key>guess:
-        return True
-    elif key<guess:
-        return False
+    if key>guess_number:
+        return 1
+    elif key<guess_number:
+        return 2
     else:
         return 0
 
-minn=1
-maxn=100
-key=10
+def main():
+    mixn = 1
+    maxn = 100
+    while True:
+        guess_number = guess(mixn,maxn)
+        result = check(guess_number)
+        if result == 0:
+            break
+        mixn,maxn = replace(mixn,maxn,guess_number,result)
+    print(f'猜测完成,结果:{guess_number}')
+main()
 
 
-i=0
-print(guess(1,2))
-'''
-while minn!=maxn:
-    guessn = median(minn,maxn)
-    if guessn<key:
-        result=True
-    elif guessn<key:
-        result=False
-    else:
-        minn=median(minn,maxn)
-        maxn=minn
-        result = minn
-    print(f'当前范围:最小值:{minn}最大值:{maxn}猜测值:{guessn}结果:{result}')
-    minn , maxn = replace(minn,maxn,guessn,result)
-print(f'最终结果:{minn}')
-'''
+        
+
+
